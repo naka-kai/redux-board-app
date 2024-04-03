c719b586e572d4a1b0e32310f572304b9b50472f
kai <sea.k.naka@gmail.com>
2024-04-03 23:54:05 +0900

    Summary

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from 'react-redux'
+import './App.css'
+import { addPost, deletePost } from './features/Posts'
+import { useState } from 'react'
 
 function App() {
+  const [name, setName] = useState('')
+  const [content, setContent] = useState('')
+  const postList = useSelector((state) => state.posts.value)
+  console.log(postList)
+
+  const dispatch = useDispatch()
+
+  const handleClick = () => {
+    dispatch(
+      addPost({
+        id: postList.length,
+        name: name,
+        content: content,
+      })
+    )
+    setName('')
+    setContent('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        <h1>React-Redux掲示板</h1>
+      </div>
+      <div>
+        <input
+          type='text'
+          placeholder='お名前'
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <input
+          type='text'
+          placeholder='投稿内容'
+          onChange={(e) => setContent(e.target.value)}
+          value={content}
+        />
+        <button onClick={() => handleClick()}>投稿</button>
+        <hr />
+      </div>
+      <div className='displayPosts'>
+        {postList.map((post) => (
+          <div key={post.id} className='post'>
+            <h1 className='postName'>{post.name}</h1>
+            <h1 className='postContent'>{post.content}</h1>
+            <button onClick={() => dispatch(deletePost({ id: post.id }))}>
+              削除
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
